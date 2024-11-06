5f58f37c4d7aa22c2318f00b256a0244f6945e34
User <trofimovsd2006@gmail.com>
2024-11-06 11:20:11 -0800

    dfs

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -8,30 +8,13 @@ from pydantic_settings import (
 )
 
 
-LOG_DEFAULT_FORMAT = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
-
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
 
-class GunicornConfig(BaseModel):
-    host: str = "0.0.0.0"
-    port: int = 8000
-    workers: int = 1
-    timeout: int = 900
 
-
-class LoggingConfig(BaseModel):
-    log_level: Literal[
-        'debug',
-        'info',
-        'warning',
-        'error',
-        'critical',
-    ] = 'info'
-    log_format: str = LOG_DEFAULT_FORMAT
 
 
 class ApiV1Prefix(BaseModel):
@@ -46,7 +29,7 @@ class ApiPrefix(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False
+    echo: bool = True
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
@@ -62,14 +45,12 @@ class DatabaseConfig(BaseModel):
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file=(".env.template", ".env"),
+        env_file=(".env"),
         case_sensitive=False,
         env_nested_delimiter="__",
         env_prefix="APP_CONFIG__",
     )
     run: RunConfig = RunConfig()
-    gunicorn: GunicornConfig = GunicornConfig()
-    logging: LoggingConfig = LoggingConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
 
--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,29 +1,30 @@
-import logging
 
+from contextlib import asynccontextmanager
 import uvicorn
 
 from core.config import settings
 
 from api import router as api_router
-from create_fastapi_app import create_app
+from fastapi import FastAPI
+
+from core.models import db_helper
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
 
 
-logging.basicConfig(
-    # level=logging.INFO
-    format=settings.logging.log_format,
-)
+app = FastAPI()
 
-main_app = create_app(
-    create_custom_static_urls=True,
-)
-
-main_app.include_router(
+app.include_router(
     api_router,
 )
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:main_app",
+        "main:app",
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
